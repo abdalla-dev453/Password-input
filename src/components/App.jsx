e2c31cb16ef98a8b14ff4906129d0ee99e3720f6
abdalla-dev453 <abdallamsema69@gmail.com>
2026-05-02 17:10:11 +0300

    modified files in the components folder

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import '../App.css'
-import PasswordInput from './PasswordInput'
-import SubmitButton from './SubmitButton'
+import "../App.css";
+import PasswordInput from "./PasswordInput";
+import SubmitButton from "./SubmitButton";
 
 function App() {
-
   return (
     <div>
-      <PasswordInput/>
-      <SubmitButton/>
+      <PasswordInput />
+      <SubmitButton />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/PasswordInput.jsx
+++ src/components/PasswordInput.jsx
@@ -1,9 +1,24 @@
 // Code PasswordInput Component Here
 
+import { useState } from "react";
+
 function PasswordInput (){
+    const [password, setPassword] = useState('')
+
+    function handleChange (e) {
+        setPassword(e.target.value)
+        console.log("Entering password...")
+    }
+
     return(
-        <></>
-    )
+        <>
+        <input 
+        type="password" 
+        value={password}
+        onChange={handleChange}
+        />
+        </>
+    );
 }
 
 export default PasswordInput;
